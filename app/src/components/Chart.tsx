ab6361fd883bd60332d79387004e225f33b8d92c
Peng Chen <pchen@Pengs-MBP.local>
2018-04-30 23:52:13 -0700

    Added axes property.

--- app/src/components/Chart.tsx
+++ app/src/components/Chart.tsx
@@ -1,30 +1,53 @@
 import * as React from "react";
 import { Point } from "@common/Interfaces";
 
+export interface Axes {
+    X: (value: number) => number;
+    Y: (value: number) => number;
+}
+
+
 export interface ChartProps extends React.Props<Chart> {
     points: Point[];
+    axes?: Axes;
 }
 
 export interface ChartState {
+    axes: Axes;
 }
 
 export class Chart extends React.Component<ChartProps, ChartState> {
     context: CanvasRenderingContext2D | null;
     container: HTMLCanvasElement | null;
 
+    defaultAxes: Axes = {
+        X: (value: number) => value,
+        Y: (value: number) => value,
+    }
+
     constructor(props: ChartProps) {
         super(props);
         this.container = null;
+        if(props.axes != null)
+        {
+            this.setState({axes: props.axes});
+        }
+        else
+        {
+            this.setState({axes: this.defaultAxes});
+        }
     }
 
     updateChart()
     {
         if(this.context != null)
         {
-            this.props.foreach(point => {
+            const context = this.context;
+            const axes = this.state.axes;
+            this.props.points.forEach(point => {
                 const x = point[0];
                 const y = point[1];
-                this.context.arc(axes.X(x), axes.Y(y), 1, 0, 2 * Math.PI);
+                context.arc(axes.X(x), axes.Y(y), 1, 0, 2 * Math.PI);
             });
         }
     }
--- app/src/components/StatsView.tsx
+++ app/src/components/StatsView.tsx
@@ -24,15 +24,6 @@ export class StatsView extends React.Component<StatsViewProps, StatsViewState> {
     render() {
         const stats = this.props.stats;
         const midSeries = stats.map(stat => [stat.x, stat.median] as Point);
-        const hiSeries = stats.map(stat => [stat.x, stat.median + stat.std] as Point);
-        const loSeries = stats.map(stat => [stat.x, stat.median - stat.std] as Point);
-        const options: Highcharts.Options = {
-            series: [
-              {data: midSeries},
-              {data: hiSeries},
-              {data: loSeries},  
-            ],
-        }
-        return <Chart options={options}/>;
+        return <Chart points={midSeries}/>;
     }
 }
